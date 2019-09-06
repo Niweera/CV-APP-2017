8c5a8caeaf057dabe97c0782bcb841aa67196ce1
nimesha95 <nimesha95@live.com>
2019-09-06 19:27:58 +0530

    removed all applicant view

---
+++ server/new_backup_script.bat
@@ -0,0 +1,12 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+set back_dir=C:\Users\amarasena\Desktop\back1\
+
+set newvar=%back_dir%%fullstamp%
+
+mongodump -h ds211648.mlab.com --port 11648 -d ucsc-cvapp-2017 -u admin -p abc123 -o %newvar%
