89c627605cba9a3ea4c0cee03d712a5b1ed3841a
Pasindu Perera <pasindubawantha@gmail.com>
2019-09-13 14:35:12 +0530

    restored startscript

--- server/startscript.js
+++ server/startscript.js
@@ -1,13 +1,2 @@
-// var cmd=require('node-cmd'); 
-// cmd.run('npm start');
-
-var execFile = require('child_process').exec;
-
-// Exec a shell script
-execFile('npm start', function(error, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    if (error !== null) {
-        console.log(`exec error: ${error}`);
-    }
-});
+var cmd=require('node-cmd'); 
+cmd.run('npm start');
