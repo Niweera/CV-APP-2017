f70520918f523243591505a435742ea660b61208
Pasindu Perera <pasindubawantha@gmail.com>
2019-09-13 14:27:34 +0530

    changed startscript to print output

--- server/startscript.js
+++ server/startscript.js
@@ -1,2 +1,13 @@
-var cmd=require('node-cmd'); 
-cmd.run('npm start');
+// var cmd=require('node-cmd'); 
+// cmd.run('npm start');
+
+var execFile = require('child_process').exec;
+
+// Exec a shell script
+execFile('npm start', function(error, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+        console.log(`exec error: ${error}`);
+    }
+});
